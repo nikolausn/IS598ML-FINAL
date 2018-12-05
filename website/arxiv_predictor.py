3f559029564ebd70027054d619d4ea966151da56
nikolausn <nikolaus.nova@gmail.com>
2018-12-04 18:35:56 -0600

    add categories definition

--- website/arxiv_predictor.py
+++ website/arxiv_predictor.py
@@ -11,6 +11,187 @@ import numpy as np
 import requests
 import re
 
+
+# class definition
+cat_dict = {
+'astro-ph':'Astrophysics',
+'cond-mat':'Condensed Matter',
+'cs':'Computer Science',
+'econ':'Economics',
+'eess':'Electrical Engineering and System Science',
+'gr-qc':'General Relativity and Quantum Cosmology',
+'hep-ex':'High Energy Physics - Experiment',
+'hep-lat':'High Energy Physics - Lattice',
+'hep-ph':'High Energy Physics - Phenomenology',
+'hep-th':'High Energy Physics - Theory',
+'math':'Mathematics',
+'math-ph':'Mathematical Physics',
+'nlin':'Nonlinear Sciences',
+'nucl-ex':'Nuclear Experiment',
+'nucl-th':'Nuclear Theory',
+'physics':'Physics',
+'q-bio':'Quantitative Biology',
+'q-fin':'Quantitative Finance',
+'quant-ph':'Quantum Physics',
+'stat':'Statistics'
+}
+
+sub_cat_dict = {
+'astro-ph':'Astrophysics',
+'astro-ph.CO':'Cosmology and Nongalactic Astrophysics',
+'astro-ph.EP':'Earth and Planetary Astrophysics',
+'astro-ph.GA':'Astrophysics of Galaxies',
+'astro-ph.HE':'High Energy Astrophysical Phenomena',
+'astro-ph.IM':'Instrumentation and Methods for Astrophysics',
+'astro-ph.SR':'Solar and Stellar Astrophysics',
+'cond-mat.dis-nn':'Disordered Systems and Neural Networks',
+'cond-mat.mes-hall':'Mesoscale and Nanoscale Physics',
+'cond-mat.mtrl-sci':'Materials Science',
+'cond-mat.other':'Other Condensed Matter',
+'cond-mat.quant-gas':'Quantum Gases',
+'cond-mat.soft':'Soft Condensed Matter',
+'cond-mat.stat-mech':'Statistical Mechanics',
+'cond-mat.str-el':'Strongly Correlated Electrons',
+'cond-mat.supr-con':'Superconductivity',
+'cs.AI':'Artificial Intelligence',
+'cs.AR':'Hardware Architecture',
+'cs.CC':'Computational Complexity',
+'cs.CE':'Computational Engineering, Finance, and Science',
+'cs.CG':'Computational Geometry',
+'cs.CL':'Computation and Language',
+'cs.CR':'Cryptography and Security',
+'cs.CV':'Computer Vision and Pattern Recognition',
+'cs.CY':'Computers and Society',
+'cs.DB':'Databases',
+'cs.DC':'Distributed, Parallel, and Cluster Computing',
+'cs.DL':'Digital Libraries',
+'cs.DM':'Discrete Mathematics',
+'cs.DS':'Data Structures and Algorithms',
+'cs.ET':'Emerging Technologies',
+'cs.FL':'Formal Languages and Automata Theory',
+'cs.GL':'General Literature',
+'cs.GR':'Graphics',
+'cs.GT':'Computer Science and Game Theory',
+'cs.HC':'Human-Computer Interaction',
+'cs.IR':'Information Retrieval',
+'cs.IT':'Information Theory',
+'cs.LG':'Machine Learning',
+'cs.LO':'Logic in Computer Science',
+'cs.MA':'Multiagent Systems',
+'cs.MM':'Multimedia',
+'cs.MS':'Mathematical Software',
+'cs.NA':'Numerical Analysis',
+'cs.NE':'Neural and Evolutionary Computing',
+'cs.NI':'Networking and Internet Architecture',
+'cs.OH':'Other Computer Science',
+'cs.OS':'Operating Systems',
+'cs.PF':'Performance',
+'cs.PL':'Programming Languages',
+'cs.RO':'Robotics',
+'cs.SC':'Symbolic Computation',
+'cs.SD':'Sound',
+'cs.SE':'Software Engineering',
+'cs.SI':'Social and Information Networks',
+'cs.SY':'Systems and Control',
+'econ.EM':'Econometrics',
+'eess.AS':'Audio and Speech Processing',
+'eess.IV':'Image and Video Processing',
+'eess.SP':'Signal Processing',
+'gr-qc':'General Relativity and Quantum Cosmology',
+'hep-ex':'High Energy Physics - Experiment',
+'hep-lat':'High Energy Physics - Lattice',
+'hep-ph':'High Energy Physics - Phenomenology',
+'hep-th':'High Energy Physics - Theory',
+'math.AC':'Commutative Algebra',
+'math.AG':'Algebraic Geometry',
+'math.AP':'Analysis of PDEs',
+'math.AT':'Algebraic Topology',
+'math.CA':'Classical Analysis and ODEs',
+'math.CO':'Combinatorics',
+'math.CT':'Category Theory',
+'math.CV':'Complex Variables',
+'math.DG':'Differential Geometry',
+'math.DS':'Dynamical Systems',
+'math.FA':'Functional Analysis',
+'math.GM':'General Mathematics',
+'math.GN':'General Topology',
+'math.GR':'Group Theory',
+'math.GT':'Geometric Topology',
+'math.HO':'History and Overview',
+'math.IT':'Information Theory',
+'math.KT':'K-Theory and Homology',
+'math.LO':'Logic',
+'math.MG':'Metric Geometry',
+'math.MP':'Mathematical Physics',
+'math.NA':'Numerical Analysis',
+'math.NT':'Number Theory',
+'math.OA':'Operator Algebras',
+'math.OC':'Optimization and Control',
+'math.PR':'Probability',
+'math.QA':'Quantum Algebra',
+'math.RA':'Rings and Algebras',
+'math.RT':'Representation Theory',
+'math.SG':'Symplectic Geometry',
+'math.SP':'Spectral Theory',
+'math.ST':'Statistics Theory',
+'math-ph':'Mathematical Physics',
+'nlin.AO':'Adaptation and Self-Organizing Systems',
+'nlin.CD':'Chaotic Dynamics',
+'nlin.CG':'Cellular Automata and Lattice Gases',
+'nlin.PS':'Pattern Formation and Solitons',
+'nlin.SI':'Exactly Solvable and Integrable Systems',
+'nucl-ex':'Nuclear Experiment',
+'nucl-th':'Nuclear Theory',
+'physics.acc-ph':'Accelerator Physics',
+'physics.ao-ph':'Atmospheric and Oceanic Physics',
+'physics.app-ph':'Applied Physics',
+'physics.atm-clus':'Atomic and Molecular Clusters',
+'physics.atom-ph':'Atomic Physics',
+'physics.bio-ph':'Biological Physics',
+'physics.chem-ph':'Chemical Physics',
+'physics.class-ph':'Classical Physics',
+'physics.comp-ph':'Computational Physics',
+'physics.data-an':'Data Analysis, Statistics and Probability',
+'physics.ed-ph':'Physics Education',
+'physics.flu-dyn':'Fluid Dynamics',
+'physics.gen-ph':'General Physics',
+'physics.geo-ph':'Geophysics',
+'physics.hist-ph':'History and Philosophy of Physics',
+'physics.ins-det':'Instrumentation and Detectors',
+'physics.med-ph':'Medical Physics',
+'physics.optics':'Optics',
+'physics.plasm-ph':'Plasma Physics',
+'physics.pop-ph':'Popular Physics',
+'physics.soc-ph':'Physics and Society',
+'physics.space-ph':'Space Physics',
+'q-bio.BM':'Biomolecules',
+'q-bio.CB':'Cell Behavior',
+'q-bio.GN':'Genomics',
+'q-bio.MN':'Molecular Networks',
+'q-bio.NC':'Neurons and Cognition',
+'q-bio.OT':'Other Quantitative Biology',
+'q-bio.PE':'Populations and Evolution',
+'q-bio.QM':'Quantitative Methods',
+'q-bio.SC':'Subcellular Processes',
+'q-bio.TO':'Tissues and Organs',
+'q-fin.CP':'Computational Finance',
+'q-fin.EC':'Economics',
+'q-fin.GN':'General Finance',
+'q-fin.MF':'Mathematical Finance',
+'q-fin.PM':'Portfolio Management',
+'q-fin.PR':'Pricing of Securities',
+'q-fin.RM':'Risk Management',
+'q-fin.ST':'Statistical Finance',
+'q-fin.TR':'Trading and Market Microstructure',
+'quant-ph':'Quantum Physics',
+'stat.AP':'Applications',
+'stat.CO':'Computation',
+'stat.ME':'Methodology',
+'stat.ML':'Machine Learning',
+'stat.OT':'Other Statistics',
+'stat.TH':'Statistics Theory'
+}
+
 with open("tfidf-feature.pickle","rb") as file:
     (tfidf_vectorizer,tfidf) = pickle.load(file)
 
@@ -248,11 +429,11 @@ def web_root():  # pragma: no cover
         for x in range(5):
             html_all += '''
                             <tr>
-                                <td>'''+str(tfidf[x][0])+'''</td><td>'''+str(tfidf[x][2])+'''</td>
-                                <td>'''+str(conv[x][0])+'''</td><td>'''+str(conv[x][2])+'''</td>
-                                <td>'''+str(lstm[x][0])+'''</td><td>'''+str(lstm[x][2])+'''</td>
+                                <td>'''+"{} ({})".format(cat_dict[tfidf[x][0]],tfidf[x][0])+'''</td><td>'''+str(tfidf[x][2])+'''</td>
+                                <td>'''+"{} ({})".format(cat_dict[conv[x][0]],conv[x][0])+'''</td><td>'''+str(conv[x][2])+'''</td>
+                                <td>'''+"{} ({})".format(cat_dict[lstm[x][0]],lstm[x][0])+'''</td><td>'''+str(lstm[x][2])+'''</td>
                             </tr>
-                    '''
+                        '''
         html_all+='''
             </table>
             <table border="1">
@@ -265,7 +446,7 @@ def web_root():  # pragma: no cover
         for x in range(10):
             html_all += '''
                             <tr>
-                                <td>'''+str(lstm_s[x][0])+'''</td><td>'''+str(lstm_s[x][2])+'''</td>
+                                <td>'''+"{} ({})".format(sub_cat_dict[lstm_s[x][0]],lstm_s[x][0])+'''</td><td>'''+str(lstm_s[x][2])+'''</td>
                             </tr>
                     '''
         html_all+='''
